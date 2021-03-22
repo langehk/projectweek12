41fc63b88b072d271a716c41d817f5fdc92e18b9
nadiaskau <nadiaskau@hotmail.com>
2021-03-22 10:26:34 +0100

    routes for admin, pending and user + session & roles

--- models/user/userHandler.js
+++ models/user/userHandler.js
@@ -3,16 +3,38 @@ const bcrypt = require("bcrypt");
 const mongooseWrap = require ('../../lib/mongooseWrap');
 //const Role = require('./user');
 
+exports.readUser = async function(req, res, query){
+  try {
+      let userinfo = await mongooseWrap.retrieve(model.User, query);
+      return userinfo;
+      
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+exports.comparePassword = async function(plain, userinfo, req){
+  //comparing plaintext (input) to hash value from database
+  const loggedin = await bcrypt.compare(plain, userinfo[0].password);
+  if(loggedin){
+      req.session.authenticated = true;       // set session vars
+      req.session.user = userinfo[0].email; 
+      req.session.role =  userinfo[0].rights; 
+  }
+
+  return loggedin;
+}
+
 exports.createUser = async function(req, res){
     let hash = await bcrypt.hash('test', 10);
 
-    let user = new model.User({
+/*     let user = new model.User({
         email: "morten@iba.dk",
         password: hash, 
         firstname: "Morten",
         lastname: "Højrup Kristensen",
         rights: model.Role.ADMIN
-      });
+      }); */
     
     await mongooseWrap.save(user); 
 }
--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,36 @@ router.get('/', async function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Express' });
+});
+
+router.get('/admin', function(req, res, next) {
+  res.render('admin', { title: 'Express' });
+});
+
+router.get('/pending', function(req, res, next) {
+  res.render('pending', { title: 'Express' });
+});
+
+router.post('/login', async function(req, res, next) { 
+  let query = {email: req.body.email};
+  handler.readUser(req, res, query).
+    then(async function(userinfo) {
+        const loggedin = await handler.comparePassword(req.body.password, userinfo, req);
+        if(loggedin && req.session.role == 'ADMIN'){
+          res.redirect('./admin');
+        }
+        if(loggedin && req.session.role == 'USER'){
+          res.redirect('../todo');
+        }
+        if(loggedin && req.session.role == 'PENDING'){
+          res.redirect('./pending');
+        }
+        else{
+          
+        }
+    });
+});
+
 module.exports = router;
