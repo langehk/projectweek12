e23cc0014ba21bd36d1e3db7374afe8f8063bd81
nadiaskau <nadiaskau@hotmail.com>
2021-03-22 10:44:47 +0100

    redirect with message

--- routes/users.js
+++ routes/users.js
@@ -11,12 +11,36 @@ router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Express' });
 });
 
+router.get('/login/:role', function(req, res, next) {
+  let message; 
+  if(req.params.role == 'pending' || req.params.role == 'user'){
+    message = 'You are not logged in, please do so:';
+  } else if(req.params.role == 'admin'){
+    message = 'You are not an admin';
+  }
+  else {
+    message = 'Create a user';
+  }
+
+  res.render('login', { message: message });
+});
+
 router.get('/admin', function(req, res, next) {
-  res.render('admin', { title: 'Express' });
+  if(req.session.authenticated && req.session.role == 'ADMIN'){
+    res.render('admin', { title: 'Express' });
+  }
+  else{
+    res.send('You are not an admin');
+  }
 });
 
 router.get('/pending', function(req, res, next) {
-  res.render('pending', { title: 'Express' });
+  if(req.session.authenticated && req.session.role == 'PENDING'){
+    res.render('pending', { title: 'Express' });
+  }
+  else{
+    res.redirect('./login/pending' )
+  }  
 });
 
 router.post('/login', async function(req, res, next) { 
