d4af77eb06ede86a2d93db0ffd96aa62d110aa48
nadiaskau <nadiaskau@hotmail.com>
2021-03-23 10:56:48 +0100

    test with xml2json

--- routes/export.js
+++ routes/export.js
@@ -3,9 +3,8 @@ var router = express.Router();
 const taskHandler = require('../models/task/taskHandler');
 const userHandler = require('../models/user/userHandler');
 const fs = require('fs').promises;
-const { DownloaderHelper } = require('node-downloader-helper'); 
-//const https = require('https'); 
-  
+const xml2js = require("xml2js");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
@@ -16,14 +15,29 @@ router.post('/', async function(req, res, next) {
   let format = req.body.format; 
   let user = await userHandler.readUser({email: userEmail}); 
   let tasks = await taskHandler.readTask({userID: user[0]._id}); 
-  tasks = JSON.stringify(tasks); 
-  await fs.writeFile(`${user[0].firstname}_${user[0].lastname}_todo.json`, tasks, function(err) {
+  tasks = JSON.stringify(tasks);
+  if(format == 'json'){
+    tasks = JSON.stringify(tasks); 
+  }
+  if(format == 'xml'){
+/*     let builder = new xml2js.Builder();
+    let xml; 
+    for (let i = 0; i < tasks.length; i++) {
+      xml += tasks[i];
+      xml += ',';
+    } */
+    let builder = new xml2js.Builder();
+    tasks = builder.buildObject(tasks);
+    console.log(tasks);
+  }
+  
+  await fs.writeFile(`${user[0].firstname}_${user[0].lastname}_todo.${format}`, tasks, function(err) {
     if (err) {
         console.log(err);
     }
   });
 
-  res.download(`${user[0].firstname}_${user[0].lastname}_todo.json`);
+  res.download(`${user[0].firstname}_${user[0].lastname}_todo.${format}`);
 
 });
 
